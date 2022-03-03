3abc7304d6e2c1668a9bebe0ca49a972b31321aa
Luiz Gomes <lgomes_kiau@hotmail.com>
2022-03-03 09:17:08 -0300

    String

---
+++ Funções para String/src/funcoesString/ToUpperCase.java
@@ -0,0 +1,17 @@
+package funcoesString;
+
+public class ToUpperCase {
+
+	public static void main(String[] args) {
+		
+		String original = "abcd FGHI ABC JHB    ";
+		
+		String s01 = original.toUpperCase();
+		
+		System.out.println("original: - " + original);
+		System.out.println("toUpperCase: - " + s01);
+
+	}
+
+}
+ 
